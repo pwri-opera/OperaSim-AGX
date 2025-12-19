2fb6f91ee16bb1d315bd8ea0d8e55dd88c3536ad
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2025-12-19 16:52:53 +0900

    hook AGXUnity simulation event for more precise time (ref #24)

--- Assets/3rdParty/AWSIM-IMU/ImuSensor.cs
+++ Assets/3rdParty/AWSIM-IMU/ImuSensor.cs
@@ -12,9 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Modified for AGXUnity by PWRI-OPERA group
+
 using Awsim.Common;
 using System;
 using UnityEngine;
+using AGXUnity;
 
 namespace Awsim.Entity
 {
@@ -62,17 +65,10 @@ namespace Awsim.Entity
         public Action<IReadOnlyOutputData> OnOutput { get; set; } = null;
 
         /// <summary>
-        /// Data output hz.
-        /// Sensor processing and callbacks are called in this hz.
-        /// </summary>
-        public int OutputHz { get => _outputHz; }
-
-        /// <summary>
         /// The bool value determines whether to gravity is considered or not.
         ///</summary>
         public bool EnableGravity { get => _enableGravity; }
 
-        [SerializeField] int _outputHz = 30;         // Autoware imu sensor basically output at 30hz.
         [SerializeField] bool _enableGravity = true;
         Vector3 _lastPosition;                       // Previous frame position used for acceleration calculation.
         Vector3 _lastVelocity;                       // Previous frame velocity used for acceleration calculation in global coordinate system.
@@ -80,6 +76,7 @@ namespace Awsim.Entity
         QuaternionD _lastRotation;                   // Previous frame rotation used for angular velocity calculation.
         OutputData _outputData = new OutputData();
         Vector3 _g;                                  // Gravity considered in measuring of acceleration and angular velocity.
+        float _prevTime;
 
         public void Initialize()
         {
@@ -94,33 +91,47 @@ namespace Awsim.Entity
             {
                 _g = Vector3.zero;
             }
-
-            // NOTE: Might be more appropriate timing to loop it in a separate thread.
-            InvokeRepeating(nameof(Output), 0f, 1.0f / OutputHz);
         }
 
-        public void Initialize(int outputHz, bool enableGravity)
+        public void Initialize(bool enableGravity)
         {
-            _outputHz = outputHz;
             _enableGravity = enableGravity;
 
             Initialize();
         }
 
-        public void OnFixedUpdate()
+        public void OnEnable()
         {
+            Simulation.Instance.StepCallbacks.SimulationPost += DoFixedUpdate;
+            _prevTime = (float)Simulation.Instance.Native.getTimeStamp();
+        }
+
+        public void OnDisable()
+        {
+            Simulation.Instance.StepCallbacks.SimulationPost -= DoFixedUpdate;
+        }
+
+        public void DoFixedUpdate()
+        {
+            var now = (float)Simulation.Instance.Native.getTimeStamp();
+            var deltaTime = now - _prevTime;
+            if (deltaTime < 0.00001f)
+            {
+                return;
+            }
+            _prevTime = now;
             // Compute angular velocity.
             var currentRotation = new QuaternionD(transform.rotation);
             var deltaRotation = currentRotation * QuaternionD.Inverse(_lastRotation);
             deltaRotation.ToAngleAxis(out var angle, out var axis);
-            var angularVelocity = (1.0f / Time.deltaTime) * (float)angle * axis;
+            var angularVelocity = (1.0f / (float)deltaTime) * (float)angle * axis;
             var localAngularVelocity = transform.InverseTransformDirection(angularVelocity);
             _lastRotation = currentRotation;
 
             // Compute acceleration.
-            var Velocity = (transform.position - _lastPosition) / Time.deltaTime;
-            var localVelocity = (transform.InverseTransformDirection(transform.position - _lastPosition)) / Time.deltaTime;
-            var localAcceleration = transform.InverseTransformDirection((Velocity - _lastVelocity) / Time.deltaTime + _g);
+            var Velocity = (transform.position - _lastPosition) / deltaTime;
+            var localVelocity = (transform.InverseTransformDirection(transform.position - _lastPosition)) / deltaTime;
+            var localAcceleration = transform.InverseTransformDirection((Velocity - _lastVelocity) / deltaTime + _g);
             _lastPosition = transform.position;
             _lastVelocity = Velocity;
             _lastLocalVelocity = localVelocity;
@@ -131,10 +142,7 @@ namespace Awsim.Entity
 
             _outputData.LinearAcceleration = localAcceleration;
             _outputData.AngularVelocity = localAngularVelocity;
-        }
 
-        void Output()
-        {
             // Calls registered callbacks.
             OnOutput?.Invoke(_outputData);
         }
