8c04ab5336fc82bfd6016100d710d780faaa3629
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2026-03-27 15:41:28 +0900

    Add frame_id fields to global pose odometry messages for bulldozer and excavator (ref #27)
    - Set odometryMsg.header.frame_id to "world" for global reference frame
    - Set odometryMsg.child_frame_id to "{MachineName}_tf/base_link" for machine-specific base link frame
    - Apply consistent frame_id configuration to both BulldozerGlobalPosePublisher and ExcavatorGlobalPosePublisher

--- Assets/Machines/Bulldozer/Scripts/ROS/BulldozerGlobalPosePublisher.cs
+++ Assets/Machines/Bulldozer/Scripts/ROS/BulldozerGlobalPosePublisher.cs
@@ -25,6 +25,9 @@ namespace PWRISimulator.ROS
 
                 GameObject trackLink = bulldozerJoint.gameObject.GetComponentInChildren<AGXUnity.Model.Track>().gameObject;
                 MessageUtil.UpdateTimeMsg(odometryMsg.header.stamp, time);
+
+                odometryMsg.header.frame_id="world";
+                odometryMsg.child_frame_id=$"{MachineName()}_tf/base_link";
                 odometryMsg.pose.pose.position = trackLink.transform.position.To<FLU>();
                 odometryMsg.pose.pose.orientation = trackLink.transform.rotation.To<FLU>();
                 previousTime = time;
